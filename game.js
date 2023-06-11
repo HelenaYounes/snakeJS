ed028d8d0c647b4d07d95aa80de4ce593d78504c
Héléna Younes <younesq.helena@gmail.com>
2023-06-11 04:15:59 +0200

    server'

---
+++ game.js
@@ -0,0 +1,203 @@
+const game = () => {
+	// const life = document.getElementById("lives");
+	// const highestScr = document.getElementById("highestScore");
+	// const scr = document.getElementById("score");
+	const canvas = document.getElementById("gameCanvas");
+	const ctx = canvas.getContext("2d");
+	const canvaSize = { width: 800, height: 600 };
+	const squareSize = 30;
+	let apple,
+		chest,
+		snake,
+		canDrawChest,
+		apples,
+		direction,
+		speed,
+		score,
+		lives,
+		highestScore,
+		gameInterval;
+
+	const snakeBody = new Image();
+	snakeBody.src = "body.png";
+	const appleImg = new Image();
+	appleImg.src = "apple.png";
+	const chestImg = new Image();
+	chestImg.src = "chest.png";
+
+	const setNewCoordinates = () => {
+		let randX =
+			Math.floor(Math.random() * (canvaSize.width / squareSize)) * squareSize;
+		let randY =
+			Math.floor(Math.random() * (canvaSize.height / squareSize)) * squareSize;
+		return { x: randX, y: randY };
+	};
+	const init = () => {
+		clearInterval(gameInterval);
+		apple = setNewCoordinates();
+		chest = setNewCoordinates();
+		snake = [setNewCoordinates()];
+		speed = 200;
+		canDrawChest = false;
+		apples = 0;
+		direction = "left";
+		score = 0;
+		lives = 0;
+
+		highestScore = Number(window.localStorage.getItem("highestscore")) || 0;
+	};
+	const getCenter = () => {
+		centerX = Math.floor(canvaSize.width / 2);
+		centerY = Math.floor(canvaSize.height / 2);
+
+		let dx = snake[0].x - centerX;
+		let dy = snake[0].y - centerY;
+		let newHead = { x: centerX, y: centerY };
+		snake.shift();
+		snake.forEach((body) => {
+			body.x = body.x - dx;
+			body.y = body.y - dy;
+		});
+		snake.unshift(newHead);
+	};
+
+	const changeDirection = () => {
+		switch (direction) {
+			case "left":
+				direction = "right";
+				break;
+			case "right":
+				direction = "left";
+				break;
+			case "up":
+				direction = "down";
+				break;
+			case "down":
+				direction = "up";
+				break;
+		}
+	};
+	const bodyCollision = () => {
+		let collided = snake.findLastIndex(
+			(body) => snake[0].x === body.x && snake[0].y === body.y
+		);
+		return collided > 0;
+	};
+
+	const isInBounds = () => {
+		let isInXaxis = snake[0].x > 0 && snake[0].x < canvaSize.width;
+		let isInYaxis = snake[0].y > 0 && snake[0].y < canvaSize.height;
+		return isInXaxis && isInYaxis;
+	};
+
+	const increaseScore = () => {
+		score += 10;
+		// scr.textContent = `${score}`;
+		apples++;
+		canDrawChest = apples > 2;
+		apple = setNewCoordinates();
+	};
+
+	const updateLives = () => {
+		lives += 1;
+		apples = 0;
+		canDrawChest = false;
+		// life.textContent = `${lives}`;
+		chest = setNewCoordinates();
+	};
+	const checkCollision = () => {
+		if (snake[0].x === apple.x && snake[0].y === apple.y) {
+			increaseScore();
+		} else if (
+			snake[0].x === chest.x &&
+			snake[0].y === chest.y &&
+			canDrawChest
+		) {
+			snake.pop();
+			updateLives();
+		} else {
+			snake.pop();
+		}
+	};
+
+	const checkGameOver = () => {
+		if (lives < 0) {
+			alert("gameOver");
+		}
+	};
+
+	const checkPosition = () => {
+		if (isInBounds() && bodyCollision()) {
+			checkCollision();
+		} else {
+			lives -= 1;
+			checkGameOver();
+		}
+	};
+	const drawApple = () => {
+		ctx.drawImage(appleImg, apple.x, apple.y);
+	};
+	const drawChest = () => {
+		if (canDrawChest) {
+			ctx.drawImage(chestImg, chest.x, chest.y);
+		}
+	};
+
+	const drawSnake = () => {
+		snake.forEach((body) => {
+			ctx.drawImage(snakeBody, body.x, body.y);
+		});
+	};
+
+	const draw = () => {
+		drawApple();
+		drawChest();
+		drawSnake();
+	};
+	const handleKeyPressed = (e) => {
+		switch (e.key) {
+			case "ArrowDown":
+				direction = direction !== "up" ? "down" : direction;
+				break;
+			case "ArrowUp":
+				direction = direction !== "down" ? "up" : direction;
+				break;
+			case "ArrowRight":
+				direction = direction !== "left" ? "right" : direction;
+				break;
+			case "ArrowLeft":
+				direction = direction !== "right" ? "left" : direction;
+				break;
+			default:
+				direction = "left";
+		}
+	};
+	const moveSnake = () => {
+		let head = { ...snake[0] };
+
+		switch (direction) {
+			case "down":
+				head = { x: head.x, y: head.y + squareSize };
+				break;
+			case "up":
+				head = { x: head.x, y: head.y - squareSize };
+				break;
+			case "right":
+				head = { x: head.x + squareSize, y: head.y };
+				break;
+			case "left":
+				head = { x: head.x - squareSize, y: head.y };
+				break;
+		}
+		snake.unshift(head);
+	};
+
+	const gameLoop = () => {
+		ctx.clearRect(0, 0, canvaSize.width, canvaSize.height);
+		draw();
+		moveSnake();
+		checkPosition();
+	};
+	document.addEventListener("keydown", handleKeyPressed);
+};
+export default game;
--- snake.js
+++ snake.js
@@ -1,240 +1,89 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
-const pauseButton = document.getElementById("startPause");
-const scr = document.getElementById("score");
-const msg = document.getElementById("game-over");
-const life = document.getElementById("lives");
-const squareSize = 30;
-const snakeBody = new Image();
-snakeBody.src = "body.png";
-const appleImg = new Image();
-appleImg.src = "apple.png";
-const chestImg = new Image();
-chestImg.src = "chest.png";
-const highestScr = document.getElementById("highestScore");
-let snake;
-let apple;
-let chest;
-let speed;
-let distance;
-let gameInterval;
-let canDrawChest;
-let gameoverMsg;
-let head;
-let apples;
-let direction = "left";
-let gamePaused;
-let score;
-let lives;
-let highestScore = Number(window.localStorage.getItem("highestscore")) || 0;
-
-const increaseSpeed = () => {
-	clearGameInterval(gameInterval);
-	speed = speed > 50 ? speed - 10 : 50;
-	gameInterval = setInterval(loopGame, speed);
-};
-const moveItem = () => ({
-	x: Math.floor(Math.random() * (canvas.width / squareSize)) * squareSize,
-	y: Math.floor(Math.random() * (canvas.height / squareSize)) * squareSize,
+import game from "./game.js";
+document.addEventListener("DOMContentLoaded", () => {
+	const start = game();
+	start.init();
+	setInterval(start.gameLoop, 200);
 });
+// // const startPause = document.getElementById("startPause");
+// // const msg = document.getElementById("game-over");
 
-const draw = () => {
-	if (canDrawChest) {
-		ctx.drawImage(chestImg, chest.x, chest.y);
-	}
-	ctx.drawImage(appleImg, apple.x, apple.y);
-	snake.forEach((body) => {
-		ctx.drawImage(snakeBody, body.x, body.y);
-	});
-};
+// // gameOver.init
+// // const updateHTML = () => {
+// // 	life.textContent = lives.toString();
+// // 	highestScr.textContent = highestScore.toString();
+// // 	scr.textContent = score.toString();
+// // };
+// // const updateHighestScore = () => {
+// // 	if (score > highestScore) {
+// // 		highestScore = score;
+// // 		highestScr.textContent = `${highestScore}`;
+// // 		localStorage.setItem("highestScore", `${highestScore}`);
+// // 	}
+// // };
+// // const clearSetTimeout = () => {
+// // 	if (!!gameoverMsg) {
+// // 		clearTimeout(gameoverMsg);
+// // 	}
+// // };
+// // const clearGameInterval = () => {
+// // 	if (!!gameInterval) {
+// // 		clearInterval(gameInterval);
+// // 	}
+// // };
+// // const gameOver = () => {
+// // 	clearTimeout();
+// // 	gamePaused = true;
+// // }
+// // 	clearGameInterval();
+// // 	msg.style.opacity = 1;
+// // 	toggleText(startPause, "New Game");
+// // 	gameoverMsg = setTimeout(init, 1000);
+// // };
 
-const isInBounds = (el) => {
-	let inYaxis = el.y >= 0 && el.y <= canvas.height;
-	let inXaxis = el.x >= 0 && el.x <= canvas.width;
-	return inXaxis && inYaxis;
-};
+// // const toggleGameOverMsg = () => {
+// // 	msg.style.opacity = 1;
+// // };
 
-const setHigherScore = () => {
-	if (score > highestScore) {
-		highestScore = score;
-		window.localStorage.setItem("highestscore", highestScore.toString());
-	}
-};
+// // const init = () => {
+// // 	msg.style.opacity = 0;
+// // 	apple = { x: initialapple.x, y: initialapple.y };
+// // 	chest = { x: initialchest.x, y: initialchest.y };
+// // 	snake = [{ x: initialsnake[0].x, y: initialsnake[0].y }];
+// // 	score = 0;
+// // 	speed = 200;
+// // 	lives = 0;
+// // 	apples = 0;
+// // 	canDrawChest = false;
+// // 	gamePaused = true;
+// // 	// updateHTML();
+// // };
 
-const updateScore = () => {
-	apples++;
-	canDrawChest = apples > 2;
-	score += 10;
-	distance += squareSize;
-	apple = moveItem();
-	setHigherScore();
-	increaseSpeed();
-};
+// // 	const increaseSpeed = () => {
+// // 		clearGameInterval(gameInterval);
+// // 		speed = speed > 50 ? speed - 10 : 50;
+// // 		gameInterval = setInterval(loopGame, speed);
+// // 	};
+// // 	const pause = () => {
+// // 		clearInterval(gameInterval);
+// // 		gamePaused = true;
+// // 		toggleText(startPause);
+// // 	};
 
-const checkGameOver = () => {
-	if (lives < 0) {
-		gameOver();
-	} else {
-		switch (direction) {
-			case "left":
-				direction = "right";
-				break;
-			case "right":
-				direction = "left";
-				break;
-			case "up":
-				direction = "down";
-				break;
-			case "down":
-				direction = "up";
-				break;
-		}
-		pause();
-	}
-};
+// // 	const toggleText = (el, str) => {
+// // 		let text = gamePaused? "Resume Game" : "Pause Game";
+// // 		el.textContent = text;
+// // 	};
+// // startPause.addEventListener("click", () => {
+// // 	gamePaused =!gamePaused
+// // 	startPause.textContent = startButtonText;
 
-const handleKeyPressed = (e) => {
-	switch (e.key) {
-		case "ArrowDown":
-			direction = direction !== "up" ? "down" : direction;
-			break;
-		case "ArrowUp":
-			direction = direction !== "down" ? "up" : direction;
-			break;
-		case "ArrowRight":
-			direction = direction !== "left" ? "right" : direction;
-			break;
-		case "ArrowLeft":
-			direction = direction !== "right" ? "left" : direction;
-			break;
-	}
-};
-const updateChest = () => {
-	lives += 1;
-	apples = 0;
-	canDrawChest = false;
-	chest = moveItem();
-};
+// // 	}
+// // 	});
+// const play = game();
+// const loop = () => {
+// 	play.draw();
+// 	play.moveSnake();
+// };
 
-const caughtItem = () => {
-	if (head.x === apple.x && head.y === apple.y) {
-		updateScore();
-	} else {
-		snake.pop();
-		if (head.x === chest.x && head.y === chest.y) {
-			updateChest();
-		} else {
-			checkCollision(head);
-		}
-	}
-	updateHTML();
-};
-
-const clearSetTimeout = () => {
-	if (!!gameoverMsg) {
-		clearTimeout(gameoverMsg);
-	}
-};
-const clearGameInterval = () => {
-	if (!!gameInterval) {
-		clearInterval(gameInterval);
-	}
-};
-const gameOver = () => {
-	clearSetTimeout();
-	gamePaused = true;
-
-	clearGameInterval();
-	msg.style.opacity = 1;
-	toggleText(pauseButton, "New Game");
-	gameoverMsg = setTimeout(init, 1000);
-};
-
-const checkCollisionBody = (el) => {
-	const headInBody = snake.some((body, index) => {
-		let collision = body.x === el.x && body.y === el.y;
-
-		return collision && index !== 0;
-	});
-};
-const checkCollision = (el) => {
-	if (!isInBounds(el) || checkCollisionBody(el)) {
-		lives -= 1;
-		checkGameOver();
-	}
-};
-
-const moveSnake = () => {
-	const { x, y } = head;
-	let newHead;
-	switch (direction) {
-		case "down":
-			newHead = { x: x, y: y + squareSize };
-
-			break;
-		case "up":
-			newHead = { x: x, y: y - squareSize };
-
-			break;
-		case "right":
-			newHead = { x: x + squareSize, y: y };
-
-			break;
-		case "left":
-			newHead = { x: x - squareSize, y: y };
-
-			break;
-	}
-	snake.unshift(newHead);
-	head = newHead;
-	caughtItem();
-};
-const updateHTML = () => {
-	life.textContent = lives.toString();
-	highestScr.textContent = highestScore.toString();
-	scr.textContent = score.toString();
-};
-const toggleText = (b, str) => {
-	b.textContent = str;
-};
-
-const pause = () => {
-	clearInterval(gameInterval);
-	gamePaused = true;
-	toggleText(pauseButton, "Resume Game");
-};
-const loopGame = () => {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	draw();
-	moveSnake();
-};
-const start = () => {
-	gamePaused = false;
-	toggleText(pauseButton, "Pause Game");
-	gameInterval = setInterval(loopGame, speed);
-};
-
-const toggleGameOverMsg = () => {
-	msg.style.opacity = 1;
-};
-
-const init = () => {
-	msg.style.opacity = 0;
-	snake = [moveItem()];
-	head = snake[0];
-	chest = moveItem();
-	apple = moveItem();
-	score = 0;
-	speed = 200;
-	lives = 0;
-	apples = 0;
-	distance = squareSize;
-	canDrawChest = false;
-	gamePaused = true;
-	updateHTML();
-};
-pauseButton.addEventListener("click", () => {
-	gamePaused ? start() : pause();
-});
-window.addEventListener("keydown", handleKeyPressed);
-init();
+// play.init();
+// setInterval(loop, 200);
